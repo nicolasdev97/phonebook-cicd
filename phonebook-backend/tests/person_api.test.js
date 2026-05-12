97dd07f32268b850b7baef4898f52a2f92799623
nicolasdev97 <nicolas.po.im@gmail.com>
2026-05-12 18:46:22 -0500

    Close mongoose connection after tests.

--- phonebook-backend/tests/person_api.test.js
+++ phonebook-backend/tests/person_api.test.js
@@ -1,11 +1,18 @@
-const request = require('supertest');
-const app = require('../app');
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
 
-describe('Phonebook API', () => {
-    test('persons are returned as json', async () => {
-        await request(app)
-            .get('/api/persons')
+const api = supertest(app);
+
+describe("Phonebook API", () => {
+    test("persons are returned as json", async () => {
+        await api
+            .get("/api/persons")
             .expect(200)
-            .expect('Content-Type', /application\/json/);
+            .expect("Content-Type", /application\/json/);
     });
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
 });
